af136c00a87858ca930264dbdcb577a0013254a8
Jos Ahrens <buughost@gmail.com>
2023-12-01 19:08:11 +0100

    initial commit

--- d01/main.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../helper/aoc-helper'
-
-sums = File.read('input.txt')
-           .split("\n\n")
-           .map { |section| section.lines.map(&:to_i).inject(:+) }
-
-puts sums.max
-puts sums.max(3).sum

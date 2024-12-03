cacc7c9ebbe1e5ad1f046c9a042c94131bc3e5f7
Jos Ahrens <jahrens@digitalocean.com>
2024-12-03 17:56:03 +0100

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 mod d01;
 
 fn main() {
-    println!("Hello, world!");
+    let day: u16 = std::env::args()
+        .nth(1)
+        .expect("Usage: ./program <day> <part>")
+        .parse()
+        .expect("day: not a digit");
+
+    println!("Day {}", day);
+    match day {
+        1 => d01::solve(),
+        _ => println!("Usage: ./program <day> <part>"),
+    }
 }
